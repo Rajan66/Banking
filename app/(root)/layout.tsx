1f1a4655e2f80b0cbd1d6fa530ab7c0569984b4a
Rajan66 <rajanmaharjan042@gmail.com>
2024-05-23 14:52:04 +0545

    fix:resolved merge conflict

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,24 +8,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const loggedIn = { firstName: "Rajan", lastName: "Maharjan" };
-  
+
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
       <div className="flex size-full flex-col">
         <div className="root-layout">
           <Image
-            src="/icons/logo.svg" 
+            src="/icons/logo.svg"
             width={30}
             height={30}
             alt="menu icon"
           />
           <div>
-            <MobileNav user={loggedIn}/>
+            <MobileNav user={loggedIn} />
             asdlkjqlwkdjqklwjdqjwdlkjqwd
+
+            <p>
+              asdkljaskldjaklsdjklasjd
+            </p>
           </div>
         </div>
-      {children}
+        {children}
       </div>
     </main>
   );
